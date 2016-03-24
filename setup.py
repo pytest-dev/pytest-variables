695241cc6cbd9e2bbbeb2cf58189f01045697f73
Dave Hunt <dave.hunt@gmail.com>
2016-03-24 09:58:29 +0000

    Add Framework::Pytest to list of classifiers

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(name='pytest-variables',
       keywords='py.test pytest json variables',
       classifiers=[
           'Development Status :: 5 - Production/Stable',
+          'Framework :: Pytest',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
           'Operating System :: POSIX',
