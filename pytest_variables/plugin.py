dbde113902b34b44f7eaa1a7e1f7afef89027835
Tom <Mcgtom10@gmail.com>
2018-01-10 12:48:50 -0800

    Use io module to encode as UTF-8
    This allows variables files to contain non-ASCII characters without throwing an error.

--- pytest_variables/plugin.py
+++ pytest_variables/plugin.py
@@ -5,6 +5,7 @@
 import os.path
 import sys
 import warnings
+import io
 
 import pytest
 from functools import reduce
@@ -14,7 +15,7 @@ from pytest_variables import errors
 
 def default(module, path):
     try:
-        with open(path, 'rb') as f:
+        with io.open(path, 'r', encoding='utf8') as f:
             return module.load(f)
     except TypeError:
         # NOTE: python 3.2-3.5 json expects string,
