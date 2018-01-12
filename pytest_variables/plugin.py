c854ea71a0ee11a392affad3bd3803a125eadbdc
Tom <Mcgtom10@gmail.com>
2018-01-11 17:08:33 -0800

    Remove try/except block as it is no longer required

--- pytest_variables/plugin.py
+++ pytest_variables/plugin.py
@@ -14,15 +14,8 @@ from pytest_variables import errors
 
 
 def default(module, path):
-    try:
-        with io.open(path, 'r', encoding='utf8') as f:
-            return module.load(f)
-    except TypeError:
-        # NOTE: python 3.2-3.5 json expects string,
-        # so we should rely on system encoding.
-        # This is fixed in newer versions.
-        with open(path) as f:
-            return module.load(f)
+    with io.open(path, 'r', encoding='utf8') as f:
+        return module.load(f)
 
 
 parser_table = {
